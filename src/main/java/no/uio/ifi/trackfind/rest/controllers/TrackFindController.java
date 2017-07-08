65e4a8bdd2dc510d9d90b7ca628a483dae95ee8d
dtitov <dmytro.titov@gmail.com>
2017-07-09 01:26:29 +0200

    Add separate methods for getting attributes and values.

--- src/main/java/no/uio/ifi/trackfind/rest/controllers/TrackFindController.java
+++ src/main/java/no/uio/ifi/trackfind/rest/controllers/TrackFindController.java
@@ -3,11 +3,14 @@ package no.uio.ifi.trackfind.rest.controllers;
 import no.uio.ifi.trackfind.services.TrackFindService;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class TrackFindController {
@@ -27,6 +30,21 @@ public class TrackFindController {
     @GetMapping(path = "/metamodel", produces = "application/json")
     public Object getMetamodel() throws IOException, ParseException {
         return trackFindService.getMetamodel().asMap();
+    }
+
+    @GetMapping(path = "/attributes", produces = "application/json")
+    public Object getAttributes(@RequestParam(required = false) String expression) throws IOException, ParseException {
+        Set<String> attributes = trackFindService.getMetamodel().asMap().keySet();
+        if (StringUtils.isEmpty(expression)) {
+            return attributes;
+        } else {
+            return attributes.stream().filter(a -> a.contains(expression)).collect(Collectors.toSet());
+        }
+    }
+
+    @GetMapping(path = "/values", produces = "application/json")
+    public Object getValues(@RequestParam String attribute) throws IOException, ParseException {
+        return trackFindService.getMetamodel().get(attribute);
     }
 
     @GetMapping(path = "/search", produces = "application/json")
