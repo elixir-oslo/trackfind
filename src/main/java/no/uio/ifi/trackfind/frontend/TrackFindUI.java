cc6c213d2f605cf36e41113f04e94f3e326e324e
dtitov <dmytro.titov@gmail.com>
2017-07-11 11:30:53 +0200

    Initial layout.

--- src/main/java/no/uio/ifi/trackfind/frontend/TrackFindUI.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/TrackFindUI.java
@@ -2,8 +2,7 @@ package no.uio.ifi.trackfind.frontend;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Tree;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import no.uio.ifi.trackfind.backend.services.TrackFindService;
 import no.uio.ifi.trackfind.frontend.data.TreeNode;
 import no.uio.ifi.trackfind.frontend.providers.TrackDataProvider;
@@ -22,9 +21,35 @@ public class TrackFindUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         Tree<TreeNode> tree = new Tree<>();
+        tree.setSelectionMode(Grid.SelectionMode.MULTI);
+        tree.setWidth(30, Unit.PERCENTAGE);
         TrackDataProvider trackDataProvider = new TrackDataProvider(new TreeNode(trackFindService.getMetamodelTree()));
         tree.setDataProvider(trackDataProvider);
-        setContent(tree);
+
+        HorizontalLayout headerLayout = new HorizontalLayout();
+
+        Panel treePanel = new Panel("Metamodel", tree);
+        treePanel.setSizeFull();
+        VerticalLayout treeLayout = new VerticalLayout(treePanel);
+        treeLayout.setSizeFull();
+        treeLayout.setExpandRatio(treePanel, 1f);
+
+        Panel midPanel = new Panel("Something");
+        VerticalLayout midLayout = new VerticalLayout(midPanel);
+
+        Panel dataPanel = new Panel("Data");
+        VerticalLayout dataLayout = new VerticalLayout(dataPanel);
+
+        HorizontalLayout mainLayout = new HorizontalLayout(treeLayout, midLayout, dataLayout);
+        mainLayout.setSizeFull();
+
+        HorizontalLayout footerLayout = new HorizontalLayout();
+
+        VerticalLayout outerLayout = new VerticalLayout(headerLayout, mainLayout, footerLayout);
+        outerLayout.setSizeFull();
+        outerLayout.setExpandRatio(mainLayout, 1f);
+
+        setContent(outerLayout);
     }
 
 }
