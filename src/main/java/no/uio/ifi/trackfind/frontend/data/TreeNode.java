f9c9f0934a4bb96a2123e5c75bd268b4cf861f51
dtitov <dmytro.titov@gmail.com>
2017-07-12 11:14:48 +0200

    Refactor TreeNode.

--- src/main/java/no/uio/ifi/trackfind/frontend/data/TreeNode.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/data/TreeNode.java
@@ -7,30 +7,37 @@ import java.util.stream.Stream;
 
 public class TreeNode implements Comparable<TreeNode> {
 
+    private TreeNode parent;
     private Map.Entry<String, Object> node;
 
     public TreeNode(Map<String, Object> root) {
+        this.parent = null;
         this.node = new AbstractMap.SimpleEntry<>(null, root);
     }
 
-    private TreeNode(Map.Entry<String, Object> node) {
+    private TreeNode(TreeNode parent, Map.Entry<String, Object> node) {
+        this.parent = parent;
         this.node = node;
+    }
+
+    public TreeNode getParent() {
+        return parent;
     }
 
     @SuppressWarnings("unchecked")
     public Stream<TreeNode> fetchChildren() {
         Object value = node.getValue();
         if (value instanceof Map) {
-            return ((Map<String, Object>) value).entrySet().stream().map(TreeNode::new);
+            return ((Map<String, Object>) value).entrySet().stream().map(e -> new TreeNode(this, e));
         } else if (value instanceof Collection) {
-            return ((Collection<String>) value).stream().map(s -> new AbstractMap.SimpleEntry<>(s, null)).map(TreeNode::new);
+            return ((Collection<String>) value).stream().map(s -> new AbstractMap.SimpleEntry<>(s, null)).map(e -> new TreeNode(this, e));
         } else {
             return Stream.empty();
         }
     }
 
-    public boolean hasChildren() {
-        return node.getValue() != null;
+    public boolean isLeaf() {
+        return node.getValue() == null;
     }
 
     @Override
