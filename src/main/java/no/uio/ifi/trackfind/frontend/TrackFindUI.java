d10e3753c174688f37973c0bee83bdd19fd081a0
dtitov <dmytro.titov@gmail.com>
2017-07-12 11:15:21 +0200

    Refactor Layouts.

--- src/main/java/no/uio/ifi/trackfind/frontend/TrackFindUI.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/TrackFindUI.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.HasValue;
-import com.vaadin.event.MouseEvents;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
@@ -13,9 +12,6 @@ import no.uio.ifi.trackfind.backend.services.TrackFindService;
 import no.uio.ifi.trackfind.frontend.data.TreeNode;
 import no.uio.ifi.trackfind.frontend.providers.TrackDataProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Collection;
-import java.util.Map;
 
 @SpringUI
 @Theme("trackfind")
@@ -32,11 +28,59 @@ public class TrackFindUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Label headerLabel = new Label("TrackFind");
-        HorizontalLayout headerLayout = new HorizontalLayout(headerLabel);
-        headerLayout.setSizeFull();
-        headerLayout.setComponentAlignment(headerLabel, Alignment.TOP_CENTER);
+        HorizontalLayout headerLayout = buildHeaderLayout();
+        VerticalLayout treeLayout = buildTreeLayout();
+        VerticalLayout queryLayout = buildQueryLayout();
+        VerticalLayout dataLayout = buildDataLayout();
+        HorizontalLayout mainLayout = buildMainLayout(treeLayout, queryLayout, dataLayout);
+        HorizontalLayout footerLayout = buildFooterLayout();
+        VerticalLayout outerLayout = buildOuterLayout(headerLayout, mainLayout, footerLayout);
+        setContent(outerLayout);
+    }
 
+    private VerticalLayout buildOuterLayout(HorizontalLayout headerLayout, HorizontalLayout mainLayout, HorizontalLayout footerLayout) {
+        VerticalLayout outerLayout = new VerticalLayout(headerLayout, mainLayout, footerLayout);
+        outerLayout.setSizeFull();
+        outerLayout.setExpandRatio(headerLayout, 0.05f);
+        outerLayout.setExpandRatio(mainLayout, 0.9f);
+        outerLayout.setExpandRatio(footerLayout, 0.05f);
+        return outerLayout;
+    }
+
+    private HorizontalLayout buildMainLayout(VerticalLayout treeLayout, VerticalLayout queryLayout, VerticalLayout dataLayout) {
+        HorizontalLayout mainLayout = new HorizontalLayout(treeLayout, queryLayout, dataLayout);
+        mainLayout.setSizeFull();
+        return mainLayout;
+    }
+
+    private VerticalLayout buildDataLayout() {
+        TextArea dataTextArea = new TextArea();
+        dataTextArea.setSizeFull();
+        dataTextArea.setReadOnly(true);
+        dataTextArea.addStyleName("scrollable-text-area");
+        Panel dataPanel = new Panel("Data", dataTextArea);
+        dataPanel.setSizeFull();
+
+        VerticalLayout dataLayout = new VerticalLayout(dataPanel);
+        dataLayout.setSizeFull();
+        return dataLayout;
+    }
+
+    private VerticalLayout buildQueryLayout() {
+        TextArea queryTextArea = new TextArea();
+        queryTextArea.setSizeFull();
+        Panel queryPanel = new Panel("Search query", queryTextArea);
+        queryPanel.setSizeFull();
+        TextField sampleQueryTextField = new TextField("Sample query", "sample_id: SRS306625_*_471 OR other_attributes>lab: U??D AND ihec_data_portal>assay: (WGB-Seq OR something)");
+        sampleQueryTextField.setEnabled(false);
+        sampleQueryTextField.setWidth(100, Unit.PERCENTAGE);
+        VerticalLayout queryLayout = new VerticalLayout(queryPanel, sampleQueryTextField);
+        queryLayout.setSizeFull();
+        queryLayout.setExpandRatio(queryPanel, 1f);
+        return queryLayout;
+    }
+
+    private VerticalLayout buildTreeLayout() {
         Tree<TreeNode> tree = new Tree<>();
         tree.setSelectionMode(Grid.SelectionMode.MULTI);
         TrackDataProvider trackDataProvider = new TrackDataProvider(new TreeNode(trackFindService.getMetamodelTree()));
@@ -53,46 +97,23 @@ public class TrackFindUI extends UI {
         VerticalLayout treeLayout = new VerticalLayout(treePanel, valuesFilterTextField);
         treeLayout.setSizeFull();
         treeLayout.setExpandRatio(treePanel, 1f);
+        return treeLayout;
+    }
 
-        TextArea queryTextArea = new TextArea();
-        queryTextArea.setSizeFull();
-        Panel queryPanel = new Panel("Search query", queryTextArea);
-        queryPanel.setSizeFull();
-        TextField sampleQueryTextField = new TextField("Sample query", "sample_id: SRS306625_*_471 OR other_attributes>lab: U??D AND ihec_data_portal>assay: (WGB-Seq OR something)");
-        sampleQueryTextField.setEnabled(false);
-        sampleQueryTextField.setWidth(100, Unit.PERCENTAGE);
-        VerticalLayout queryLayout = new VerticalLayout(queryPanel, sampleQueryTextField);
-        queryLayout.setSizeFull();
-        queryLayout.setExpandRatio(queryPanel, 1f);
-
-        TextArea dataTextArea = new TextArea();
-        dataTextArea.setSizeFull();
-        dataTextArea.setReadOnly(true);
-        dataTextArea.addStyleName("scrollable-text-area");
-        Panel dataPanel = new Panel("Data", dataTextArea);
-        dataPanel.setSizeFull();
-        queryPanel.addClickListener((MouseEvents.ClickListener) event -> {
-            Collection<Map> result = trackFindService.search(sampleQueryTextField.getValue());
-            dataTextArea.setValue(gson.toJson(result));
-        });
-        VerticalLayout dataLayout = new VerticalLayout(dataPanel);
-        dataLayout.setSizeFull();
-
-        HorizontalLayout mainLayout = new HorizontalLayout(treeLayout, queryLayout, dataLayout);
-        mainLayout.setSizeFull();
-
+    private HorizontalLayout buildFooterLayout() {
         Label footerLabel = new Label("2017");
         HorizontalLayout footerLayout = new HorizontalLayout(footerLabel);
         footerLayout.setSizeFull();
         footerLayout.setComponentAlignment(footerLabel, Alignment.BOTTOM_CENTER);
+        return footerLayout;
+    }
 
-        VerticalLayout outerLayout = new VerticalLayout(headerLayout, mainLayout, footerLayout);
-        outerLayout.setSizeFull();
-        outerLayout.setExpandRatio(headerLayout, 0.05f);
-        outerLayout.setExpandRatio(mainLayout, 0.9f);
-        outerLayout.setExpandRatio(footerLayout, 0.05f);
-
-        setContent(outerLayout);
+    private HorizontalLayout buildHeaderLayout() {
+        Label headerLabel = new Label("TrackFind");
+        HorizontalLayout headerLayout = new HorizontalLayout(headerLabel);
+        headerLayout.setSizeFull();
+        headerLayout.setComponentAlignment(headerLabel, Alignment.TOP_CENTER);
+        return headerLayout;
     }
 
 }
