7bf864a8f92f15df17644bab40061d033b5bfd37
dtitov <dmytro.titov@gmail.com>
2017-07-27 14:33:01 +0200

    Add data front-end JavaDocs.

--- src/main/java/no/uio/ifi/trackfind/frontend/data/TreeNode.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/data/TreeNode.java
@@ -5,32 +5,61 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Stream;
 
+/**
+ * Data type for representing metadata element (attribute or value) on front-end.
+ */
 public class TreeNode implements Comparable<TreeNode> {
 
     private int level;
     private TreeNode parent;
     private Map.Entry<String, Object> node;
 
+    /**
+     * Constructs TreeNode instance of root element, setting level to zero and parent tu null.
+     *
+     * @param root Root element of the metadata hierarchy.
+     */
     public TreeNode(Map<String, Object> root) {
         this.level = 0;
         this.parent = null;
         this.node = new AbstractMap.SimpleEntry<>(null, root);
     }
 
+    /**
+     * Constructs TreeNode for some child element, accepting it and its parent.
+     *
+     * @param parent Parent TreeNode for this child element.
+     * @param node   Child element.
+     */
     private TreeNode(TreeNode parent, Map.Entry<String, Object> node) {
         this.parent = parent;
         this.node = node;
         this.level = parent.getLevel() + 1;
     }
 
+    /**
+     * Gets level of current node in the metamodel hierarchy.
+     *
+     * @return Level of current node.
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Gets parent node for current node.
+     *
+     * @return Parent TreeNode.
+     */
     public TreeNode getParent() {
         return parent;
     }
 
+    /**
+     * Fetches children of current node.
+     *
+     * @return Current node's children (attributes or values).
+     */
     @SuppressWarnings("unchecked")
     public Stream<TreeNode> fetchChildren() {
         Object value = node.getValue();
@@ -43,6 +72,11 @@ public class TreeNode implements Comparable<TreeNode> {
         }
     }
 
+    /**
+     * Gets path from the root node to current node.
+     *
+     * @return Sequence of attributes separated by some delimiter (">" by default).
+     */
     public String getPath() {
         String path = "";
         TreeNode parent = getParent();
@@ -57,29 +91,51 @@ public class TreeNode implements Comparable<TreeNode> {
         }
     }
 
+    /**
+     * Checks if it's a leaf-node (value of the metamodel).
+     *
+     * @return true if it's a leaf-node, false otherwise.
+     */
     public boolean isLeaf() {
         return node.getValue() == null;
     }
 
+    /**
+     * Checks if it's a final attribute (containing no children attributes, but values only).
+     *
+     * @return true if it's a final attribute, false otherwise.
+     */
     public boolean isFinalAttribute() {
         return fetchChildren().anyMatch(TreeNode::isLeaf);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int compareTo(TreeNode that) {
         return String.valueOf(this).compareTo(String.valueOf(that));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return node.getKey();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return this.toString().hashCode();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object obj) {
         return obj instanceof TreeNode && this.toString().equals(obj.toString());
