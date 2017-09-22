766f0603f0d0ca2ea670ec98b5b936706d1a5ec8
Dmytro Titov <dmytro.titov@gmail.com>
2017-09-22 14:50:57 +0200

    Migrate to TreeDataProvider.

--- src/main/java/no/uio/ifi/trackfind/frontend/listeners/TreeSelectionListener.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/listeners/TreeSelectionListener.java
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
  *
  * @author Dmytro Titov
  */
-// TODO: Doesn't work, fix it.
 public class TreeSelectionListener implements SelectionListener<TreeNode> {
 
     private Tree<TreeNode> tree;
