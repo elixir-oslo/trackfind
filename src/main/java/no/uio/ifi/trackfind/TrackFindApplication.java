88641d53d01c4b33ed89107f499cb0223a04be78
Dmytro Titov <dmytro.titov@gmail.com>
2017-09-23 20:25:03 +0200

    Gson#disableHtmlEscaping()

--- src/main/java/no/uio/ifi/trackfind/TrackFindApplication.java
+++ src/main/java/no/uio/ifi/trackfind/TrackFindApplication.java
@@ -3,23 +3,13 @@ package no.uio.ifi.trackfind;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
-import java.util.Collections;
 
 /**
  * Spring Boot application's main class with some configuration and some beans defined.
@@ -32,8 +22,6 @@ import java.util.Collections;
 public class TrackFindApplication {
 
     public static final String INDICES_FOLDER = "indices/";
-
-    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     private ObjectMapper objectMapper;
 
@@ -49,33 +37,6 @@ public class TrackFindApplication {
     private void configureObjectMapper() {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-    }
-
-    @Bean
-    public Gson gson() {
-        return new GsonBuilder().setPrettyPrinting().setDateFormat(DATE_FORMAT).create();
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).
-                useDefaultResponseMessages(false).
-                select().
-                apis(RequestHandlerSelectors.basePackage("no.uio.ifi.trackfind.backend.rest.controllers")).
-                paths(PathSelectors.any()).
-                build().
-                apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo("TrackFind",
-                "Search engine for finding genomic tracks",
-                String.valueOf(getClass().getPackage().getImplementationVersion()),
-                null,
-                new Contact(null, "https://github.com/elixir-no-nels/trackfind", null),
-                null,
-                null,
-                Collections.emptyList());
     }
 
     @Autowired
--- src/main/java/no/uio/ifi/trackfind/backend/configuration/BeanDefinitions.java
+++ src/main/java/no/uio/ifi/trackfind/backend/configuration/BeanDefinitions.java
@@ -1,13 +1,22 @@
 package no.uio.ifi.trackfind.backend.configuration;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,6 +24,13 @@ import static no.uio.ifi.trackfind.TrackFindApplication.INDICES_FOLDER;
 
 @Configuration
 public class BeanDefinitions {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().setDateFormat(DATE_FORMAT).create();
+    }
 
     @Bean
     public Git git() throws IOException, GitAPIException {
@@ -26,6 +42,28 @@ public class BeanDefinitions {
             git.commit().setMessage("Initial commit.").call();
         }
         return git;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).
+                useDefaultResponseMessages(false).
+                select().
+                apis(RequestHandlerSelectors.basePackage("no.uio.ifi.trackfind.backend.rest.controllers")).
+                paths(PathSelectors.any()).
+                build().
+                apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("TrackFind",
+                "Search engine for finding genomic tracks",
+                String.valueOf(getClass().getPackage().getImplementationVersion()),
+                null,
+                new Contact(null, "https://github.com/elixir-no-nels/trackfind", null),
+                null,
+                null,
+                Collections.emptyList());
     }
 
     @Bean
