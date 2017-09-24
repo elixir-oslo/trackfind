946ecfbb8e1c3dcd824e230b432f5171d742cdff
Dmytro Titov <dmytro.titov@gmail.com>
2017-09-25 00:29:16 +0200

    Add RestResponseEntityExceptionHandler.

---
+++ src/main/java/no/uio/ifi/trackfind/backend/rest/handlers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,20 @@
+package no.uio.ifi.trackfind.backend.rest.handlers;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @SuppressWarnings("unused")
+    @ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<Object> handle(Exception exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+}
