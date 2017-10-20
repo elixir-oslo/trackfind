31d8d5a8811b5ff255e9ba8c2453878c7cc2d33b
Dmytro Titov <dmytro.titov@gmail.com>
2017-10-20 14:59:23 +0200

    Fix ICGC's "two-steps download".

--- src/main/java/no/uio/ifi/trackfind/backend/data/providers/icgc/ICGCDataProvider.java
+++ src/main/java/no/uio/ifi/trackfind/backend/data/providers/icgc/ICGCDataProvider.java
@@ -13,7 +13,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -28,7 +27,6 @@ public class ICGCDataProvider extends PaginationAwareDataProvider { // TODO: Fet
     private static final String DONORS = "https://dcc.icgc.org/api/v1/donors?";
     private static final String SUBMIT = "https://dcc.icgc.org/api/v1/download/submit?filters={%22donor%22:{%22id%22:{%22is%22:[%22__DONOR_ID__%22]}}}&info=[{%22key%22:%22__DATA_TYPE__%22,%22value%22:%22TSV%22}]";
     private static final String DOWNLOAD = "https://dcc.icgc.org/api/v1/download/";
-    private static final String AVAILABLE_DATA_TYPES = "availableDataTypes";
 
     /**
      * {@inheritDoc}
@@ -47,37 +45,36 @@ public class ICGCDataProvider extends PaginationAwareDataProvider { // TODO: Fet
     @Override
     protected void postProcessPage(Collection<Map> page) {
         for (Map dataset : page) {
-            Map<String, Collection<String>> browser = new HashMap<>();
+            Map<String, String> browser = new HashMap<>();
             String donorId = (String) dataset.get("id");
-            Collection<String> availableDataTypes = (Collection<String>) dataset.get(AVAILABLE_DATA_TYPES);
+            Collection<String> availableDataTypes = (Collection<String>) dataset.get("availableDataTypes");
             for (String dataType : availableDataTypes) {
-                browser.computeIfAbsent(dataType, k -> new HashSet<>()).add(SUBMIT.replace("__DONOR_ID__", donorId).replace("__DATA_TYPE__", dataType));
+                browser.put(dataType, SUBMIT.replace("__DONOR_ID__", donorId).replace("__DATA_TYPE__", dataType));
             }
             dataset.put("browser", browser);
-            dataset.remove(AVAILABLE_DATA_TYPES);
         }
     }
 
     /**
      * {@inheritDoc}
      */
+    @SuppressWarnings("unchecked")
     @Override // hack for ICGC's "two-steps download"
     public Multimap<String, Map> search(String query, int limit) {
-//        String separator = properties.getLevelsSeparator();
-//        String advancedURL = properties.getAdvancedSectionName() + separator + properties.getDataURLAttribute();
         Multimap<String, Map> result = super.search(query, limit);
-//        for (Map map : result.values()) {
-//            Dynamic dynamic = Dynamic.from(map);
-//            String submitURL = dynamic.get(advancedURL, separator).asString();
-//            try (InputStream inputStream = new URL(submitURL).openStream();
-//                 InputStreamReader reader = new InputStreamReader(inputStream)) {
-//                Download download = gson.fromJson(reader, Download.class);
-//                dynamic.get(properties.getAdvancedSectionName(), separator).asMap().put(properties.getDataURLAttribute(), DOWNLOAD + download.getDownloadId());
-//            } catch (IOException e) {
-//                log.error(e.getMessage(), e);
-//                return null;
-//            }
-//        }
+        for (Map map : result.values()) {
+            Map<String, String> browser = Dynamic.from(map).get("Advanced>browser", properties.getLevelsSeparator()).asMap();
+            for (String dataType : browser.keySet()) {
+                String submitURL = browser.get(dataType);
+                try (InputStream inputStream = new URL(submitURL).openStream();
+                     InputStreamReader reader = new InputStreamReader(inputStream)) {
+                    Download download = gson.fromJson(reader, Download.class);
+                    browser.put(dataType, DOWNLOAD + download.getDownloadId());
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
         return result;
     }
 
