b4ead4ac6e7a3ca9182798ea15823b8f326c41c6
Dmytro Titov <dmytro.titov@gmail.com>
2018-10-30 16:28:48 +0100

    Add version to fair_content.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,18 +28,27 @@ CREATE INDEX IF NOT EXISTS standard_content_index
   USING gin (content);
 
 CREATE OR REPLACE VIEW datasets AS
-  SELECT source.id                                                                                                    AS id,
-         source.repository                                                                                            AS repository,
-         source.content                                                                                               AS curated_content,
-         standard.content                                                                                             AS standard_content,
-         source.content ||
-         JSONB_BUILD_OBJECT('fair', JSONB_SET(COALESCE(standard.content, '{}'::jsonb), '{id}', TO_JSONB(source.id)))  AS fair_content,
-         source.raw_version                                                                                           AS raw_version,
-         source.curated_version                                                                                       AS curated_version,
-         COALESCE(standard.version, 0)                                                                                AS standard_version,
-         CONCAT(source.raw_version, ':', source.curated_version, ':', COALESCE(standard.version, 0))                  AS version
-  FROM source
-         LEFT JOIN standard on source.id = standard.id;
+  SELECT
+         sub_query.repository,
+         sub_query.curated_content,
+         sub_query.standard_content,
+         JSONB_SET(JSONB_SET(sub_query.fair_content, '{fair, id}', TO_JSONB(sub_query.id)), '{fair, version}', TO_JSONB(sub_query.version)) AS fair_content,
+         sub_query.raw_version,
+         sub_query.curated_version,
+         sub_query.standard_version,
+         sub_query.version
+  FROM (SELECT
+               source.id                                                                                                    AS id,
+               source.repository                                                                                            AS repository,
+               source.content                                                                                               AS curated_content,
+               standard.content                                                                                             AS standard_content,
+               source.content || JSONB_BUILD_OBJECT('fair', COALESCE(standard.content, '{}'::jsonb))                        AS fair_content,
+               source.raw_version                                                                                           AS raw_version,
+               source.curated_version                                                                                       AS curated_version,
+               COALESCE(standard.version, 0)                                                                                AS standard_version,
+               CONCAT(source.raw_version, ':', source.curated_version, ':', COALESCE(standard.version, 0))                  AS version
+        FROM source
+               LEFT JOIN standard on source.id = standard.id) AS sub_query;
 
 CREATE TABLE IF NOT EXISTS mappings
 (
