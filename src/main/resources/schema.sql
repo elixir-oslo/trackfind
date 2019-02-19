9c7948122667ec8ee6f86cf96a3af21ba36c9ba6
Dmytro Titov <dmytro.titov@gmail.com>
2019-02-19 12:24:49 +0100

    Add jsonb_recursive_merge SQL function, remove unused queries.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,20 @@
+CREATE OR REPLACE FUNCTION jsonb_recursive_merge(a jsonb, b jsonb)
+  RETURNS jsonb
+  LANGUAGE SQL AS
+$$
+SELECT jsonb_object_agg(
+           COALESCE(ka, kb),
+           CASE
+             WHEN va ISNULL THEN vb
+             WHEN vb ISNULL THEN va
+             WHEN jsonb_typeof(va) <> 'object' THEN vb
+             ELSE jsonb_recursive_merge(va, vb)
+             END
+         )
+FROM jsonb_each(a) e1(ka, va)
+       FULL JOIN jsonb_each(b) e2(kb, vb) ON ka = kb
+$$;
+
 CREATE TABLE IF NOT EXISTS hubs
 (
   repository   VARCHAR(255) NOT NULl,
