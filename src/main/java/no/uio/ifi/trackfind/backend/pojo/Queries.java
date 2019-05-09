6871d997537c533674f9104cef9c458a7f9540d2
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-09 14:31:19 +0200

    Search user can only read tf_latest_objects

--- src/main/java/no/uio/ifi/trackfind/backend/pojo/Queries.java
+++ src/main/java/no/uio/ifi/trackfind/backend/pojo/Queries.java
@@ -10,6 +10,6 @@ public interface Queries {
 
     String CHECK_SEARCH_USER_EXISTS = "SELECT count(*) FROM pg_catalog.pg_roles WHERE rolname = 'search'";
 
-    String CREATE_SEARCH_USER = "CREATE USER search PASSWORD 'search'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO search;";
+    String CREATE_SEARCH_USER = "CREATE USER search PASSWORD 'search'; GRANT SELECT ON tf_latest_objects TO search;";
 
 }
