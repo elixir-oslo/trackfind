ccc1984fe2d62332439ef6da546ea5d4a2bdd8df
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-13 13:13:21 +0200

    Align ExampleDataProvider with new schema

--- src/main/java/no/uio/ifi/trackfind/backend/data/providers/example/ExampleDataProvider.java
+++ src/main/java/no/uio/ifi/trackfind/backend/data/providers/example/ExampleDataProvider.java
@@ -1,5 +1,6 @@
 package no.uio.ifi.trackfind.backend.data.providers.example;
 
+import com.google.common.collect.HashMultimap;
 import lombok.extern.slf4j.Slf4j;
 import no.uio.ifi.trackfind.backend.data.providers.AbstractDataProvider;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Draft of Data Provider for TrackHubRegistry.
@@ -18,7 +21,7 @@ import java.util.Collection;
 @Component
 public class ExampleDataProvider extends AbstractDataProvider {
 
-    private static final String FETCH_URL = "https://hyperbrowser.uio.no/hb/static/hyperbrowser/files/trackfind/basic_example.json";
+    private static final String FETCH_URL = "https://raw.githubusercontent.com/fairtracks/fairtracks_standard/master/json/examples/fairtracks.example.json";
 
     /**
      * {@inheritDoc}
@@ -26,12 +29,18 @@ public class ExampleDataProvider extends AbstractDataProvider {
     @SuppressWarnings("unchecked")
     @Override
     protected void fetchData(String hubName) {
-//        try (InputStream inputStream = new URL(FETCH_URL).openStream();
-//             InputStreamReader reader = new InputStreamReader(inputStream)) {
-//            save(hubName, gson.fromJson(reader, Collection.class));
-//        } catch (Exception e) {
-//            log.error(e.getMessage(), e);
-//        }
+        HashMultimap<String, String> mapToSave = HashMultimap.create();
+        try (InputStream inputStream = new URL(FETCH_URL).openStream();
+             InputStreamReader reader = new InputStreamReader(inputStream)) {
+            Map topMap = gson.fromJson(reader, Map.class);
+            mapToSave.put("studies_Example", gson.toJson(((List)topMap.get("studies")).get(0)));
+            mapToSave.put("experiments_Example", gson.toJson(((List)topMap.get("experiments")).get(0)));
+            mapToSave.put("samples_Example", gson.toJson(((List)topMap.get("samples")).get(0)));
+            mapToSave.put("tracks_Example", gson.toJson(((List)topMap.get("tracks")).get(0)));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        save(hubName, mapToSave.asMap());
     }
 
 }
