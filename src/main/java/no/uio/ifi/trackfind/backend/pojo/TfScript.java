507ff769eeb7fd0030f9c1bc91b0100ebfc5a532
Dmytro Titov <dmytro.titov@gmail.com>
2019-06-12 12:30:31 +0200

    TfScript -> Serializable

--- src/main/java/no/uio/ifi/trackfind/backend/pojo/TfScript.java
+++ src/main/java/no/uio/ifi/trackfind/backend/pojo/TfScript.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tf_scripts")
 @Data
 @NoArgsConstructor
-public class TfScript {
+public class TfScript implements Serializable {
 
     @Id
     @Column(name = "id", nullable = false)
