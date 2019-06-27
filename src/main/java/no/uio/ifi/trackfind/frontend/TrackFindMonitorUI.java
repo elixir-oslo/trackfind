f96bc1cb343fbc52b97f5a7ffd1f75515b036588
Dmytro Titov <dmytro.titov@gmail.com>
2019-06-27 10:44:31 +0200

    Create Vaadin view with BrowserFrame with Hystrix dashboard

--- src/main/java/no/uio/ifi/trackfind/frontend/TrackFindMonitorUI.java
+++ src/main/java/no/uio/ifi/trackfind/frontend/TrackFindMonitorUI.java
@@ -3,24 +3,11 @@ package no.uio.ifi.trackfind.frontend;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.Widgetset;
-import com.vaadin.data.HasValue;
-import com.vaadin.data.provider.AbstractBackEndDataProvider;
-import com.vaadin.data.provider.Query;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
-import com.vaadin.ui.renderers.ButtonRenderer;
-import com.vaadin.ui.renderers.ClickableRenderer;
-import elemental.json.JsonValue;
 import lombok.extern.slf4j.Slf4j;
-import no.uio.ifi.trackfind.backend.pojo.TfHub;
-import no.uio.ifi.trackfind.backend.pojo.TfObjectType;
-import no.uio.ifi.trackfind.backend.pojo.TfReference;
-import no.uio.ifi.trackfind.backend.services.MetamodelService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.stream.Stream;
 
 /**
  * Hystrix Vaadin UI of the application.
@@ -39,14 +26,14 @@ public class TrackFindMonitorUI extends AbstractUI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         HorizontalLayout headerLayout = buildHeaderLayout();
-        VerticalLayout referencesLayout = buildReferencesLayout();
+        VerticalLayout referencesLayout = buildHystrixLayout();
         HorizontalLayout mainLayout = buildMainLayout(referencesLayout);
         HorizontalLayout footerLayout = buildFooterLayout();
         VerticalLayout outerLayout = buildOuterLayout(headerLayout, mainLayout, footerLayout);
         setContent(outerLayout);
     }
 
-    private VerticalLayout buildReferencesLayout() {
+    private VerticalLayout buildHystrixLayout() {
         VerticalLayout layout = new VerticalLayout();
         layout.setSizeFull();
         BrowserFrame browser = new BrowserFrame("Browser", new ExternalResource("http://localhost/hystrix/monitor?stream=http%3A%2F%2Flocalhost%2Factuator%2Fhystrix.stream&title=TrackFind"));
