cef5abdabb5ad4cd19212ec89af0b08e80509eb5
Dmytro Titov <dmytro.titov@gmail.com>
2019-07-01 20:16:37 +0200

    Add more int tests

--- int-tests/src/test/kotlin/no/uio/ifi/inttests/Tests.kt
+++ int-tests/src/test/kotlin/no/uio/ifi/inttests/Tests.kt
@@ -31,10 +31,70 @@ class Tests {
     }
 
     @Test
-    fun testMetamodel() {
+    fun testGetMetamodel() {
         given().`when`().get("/metamodel/$EXAMPLE/$EXAMPLE").then().statusCode(200).assertThat()
             .body(
                 matchesJsonSchemaInClasspath("fairtracks.schema.json")
+            )
+    }
+
+    @Test
+    fun testGetCategories() {
+        given().`when`().get("/categories/$EXAMPLE/$EXAMPLE").then().statusCode(200).assertThat()
+            .body("", hasItems("tracks", "experiments", "samples", "studies", "non_standard_samples"))
+    }
+
+    @Test
+    fun testGetAttributes() {
+        given().`when`().get("/attributes/$EXAMPLE/$EXAMPLE/samples").then().statusCode(200).assertThat()
+            .body(
+                "",
+                hasItems(
+                    "local_id",
+                    "global_id",
+                    "biomaterial_type",
+                    "sample_type"
+                )
+            )
+    }
+
+    @Test
+    fun testGetSubAttributes() {
+        given().`when`().get("/attributes/$EXAMPLE/$EXAMPLE/samples?path=sample_type").then()
+            .statusCode(200)
+            .assertThat()
+            .body(
+                "",
+                hasItems(
+                    "term_value",
+                    "term_iri"
+                )
+            )
+    }
+
+    @Test
+    fun testGetValues() {
+        given().`when`().get("/values/$EXAMPLE/$EXAMPLE/samples?path=sample_type->term_value").then()
+            .statusCode(200)
+            .assertThat()
+            .body(
+                "",
+                hasItems(
+                    "H1-hESC"
+                )
+            )
+    }
+
+    @Test
+    fun testGetValuesWithFilter() {
+        given().`when`().get("/values/$EXAMPLE/$EXAMPLE/tracks?path=file_format->term_value&filter=ENCODE").then()
+            .statusCode(200)
+            .assertThat()
+            .body(
+                "",
+                hasItems(
+                    "ENCODE narrow peak format"
+                )
             )
     }
 
